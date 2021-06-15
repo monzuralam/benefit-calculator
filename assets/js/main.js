343ab76304a0672a61c5ad616f37ddb157bce6fa
monzuralam <monzur707@gmail.com>
2021-06-15 16:19:19 +0600

    magnific popup js init for video

--- assets/js/main.js
+++ assets/js/main.js
@@ -146,3 +146,23 @@ jQuery(function ($) {
   $this.countTo(options);
   }
 });
+
+$('.video').magnificPopup({
+  type: 'iframe',
+  
+  
+  iframe: {
+     markup: '<div class="mfp-iframe-scaler">'+
+                '<div class="mfp-close"></div>'+
+                '<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+                '<div class="mfp-title"></div>'+
+              '</div>'
+  },
+  callbacks: {
+    markupParse: function(template, values, item) {
+     values.title = item.el.attr('title');
+    }
+  }
+  
+  
+});
